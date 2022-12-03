5857e4e0e40da9097d78566a420905ad542c733c
Valentina <valentinagabricc2@gmail.com>
2022-12-03 17:04:28 +0100

    3.12

---
+++ Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR39_2021_POP2022.Models
+{
+    [Serializable]
+    class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string StreetNumber { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Street} {StreetNumber}, {City}, {Country}";
+        }
+    }
+}
+
